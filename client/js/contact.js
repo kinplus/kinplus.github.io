838b57a5b8b85d4464208e75829d724ac47b8b54
fadlykayo <f_kayo@yahoo.com>
2018-06-28 01:47:29 +0700

    Edit part 10

--- client/js/contact.js
+++ client/js/contact.js
@@ -1,22 +1,24 @@
-var map;
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -6.217245, lng: 106.761033},
-  zoom: 8
-});
+$(document).ready(function() {
+  var map;
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -6.217245, lng: 106.761033},
+    zoom: 8
+  });
 
-var marker;
-marker = new google.maps.Marker({
-  position: new google.maps.LatLng( -6.217245,106.761033),
-  map: map,
-  title: 'KIN+ Sports Tape'
-});
+  var marker;
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng( -6.217245,106.761033),
+    map: map,
+    title: 'KIN+ Sports Tape'
+  });
 
-var contentString = '<p>KIN+ Sports Tape</p>';
-var infowindow;
-infowindow = new google.maps.InfoWindow({
-  content: contentString
-});
+  var contentString = '<p>KIN+ Sports Tape</p>';
+  var infowindow;
+  infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
 
-google.maps.event.addListener(marker, 'click', function() {
-  infowindow.open(map,marker);
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
 });

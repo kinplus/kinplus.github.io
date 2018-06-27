fd7216a104aef54f73ea6d17c9c923585b0b87d6
Fadly Kayo <fadly.kayo@googlemail.com>
2018-06-27 10:10:09 +0700

    Edit part 5

--- client/js/contact.js
+++ client/js/contact.js
@@ -1,14 +1,22 @@
-var map = new GMaps({
-  el: '#map',
-  lat: -6.217245,
-  lng: 106.761033
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -6.217245, lng: 106.761033},
+    zoom: 8
+  });
+}
+
+var marker = new google.maps.Marker({
+  position: new google.maps.LatLng( -6.217245,106.761033),
+  map: map,
+  title: 'KIN+ Sports Tape'
 });
 
-map.addMarker({
-  lat: -6.217245,
-  lng: 106.761033,
-  title: 'KIN+ Sports Tape',
-  infoWindow: {
-    content: '<p>KIN+ Sports Tape</p>'
-  }
+var contentString = '<p>KIN+ Sports Tape</p>';
+var infowindow = new google.maps.InfoWindow({
+  content: contentString
+});
+
+google.maps.event.addListener(marker, 'click', function() {
+  infowindow.open(map,marker);
 });

f52d2dad2bba017b8402cf22ad128e7520a3408d
fadlykayo <f_kayo@yahoo.com>
2018-06-29 00:45:02 +0700

    Edit map

--- client/js/contact.js
+++ client/js/contact.js
@@ -1,24 +1,25 @@
   var map;
+  var marker;
+  var contentString = '<p>KIN+ Sports Tape</p>';
+  var infowindow;
+  
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -6.217245, lng: 106.761033},
       zoom: 20
     });
+
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng( -6.217245,106.761033),
+      map: map,
+      title: 'KIN+ Sports Tape'
+    });
+
+    infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+    });
   }
-
-  var marker;
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng( -6.217245,106.761033),
-    map: map,
-    title: 'KIN+ Sports Tape'
-  });
-
-  var contentString = '<p>KIN+ Sports Tape</p>';
-  var infowindow;
-  infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
